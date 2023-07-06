8d713c03dd40ed112bfc15dc775ff3c4efa13fef
JeraiLuzada <20213477@s.ubaguio.edu>
2023-07-06 11:07:23 +0800

    July6

--- finals/src/pages/header.js
+++ finals/src/pages/header.js
@@ -5,14 +5,16 @@ import header1 from './Header.png';
 function Header() {
   return (
     <div className="App">
-      <header className="Header">
-        <div>
-            <h1 className='sus-dev'>Sustainable Development</h1>
-            <h1 className='goal5'>Goal 5: Gender Equality</h1>
-            <p className='header-desc'>Goal 5 of the 17 Sustainable Development Goals, established by the United Nations in 2015, focuses on achieving gender equality and acknowledges that development should encompass social, economic, and environmental sustainability.</p>
-        </div>
-        <div>
-            <img className='header-png' src={header1}/>
+      <header className="Header mt-5 ms-5">
+        <div className='row'>
+          <div className='col-6'>
+              <h1 className='sus-dev'>Sustainable Development</h1>
+              <h1 className='goal5'>Goal 5: Gender Equality</h1>
+              <p className='header-desc'>Goal 5 of the 17 Sustainable Development Goals, established by the United Nations in 2015, focuses on achieving gender equality and acknowledges that development should encompass social, economic, and environmental sustainability.</p>
+          </div>
+          <div className='col-6'>
+              <img className='header-png' src={header1}/>
+          </div>
         </div>
       </header>
     </div>
