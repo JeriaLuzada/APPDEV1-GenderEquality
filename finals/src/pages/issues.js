fe259ec45287d804087a4168de6a47b91d8d607a
VallereeDJoseph <20212590@s.ubaguio.edu>
2023-07-10 10:47:49 +0800

    issues

---
+++ finals/src/pages/issues.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import IssuesComponents from './issuesComponents'
+import Navbar from '../components/navbar'
+function issues() {
+  return (
+    <div>
+        <Navbar />
+        <IssuesComponents/>
+        </div>
+  )
+}
+
+export default issues
