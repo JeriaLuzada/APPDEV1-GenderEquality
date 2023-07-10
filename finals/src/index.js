2b78f8faed97b9598c74bd86c8587e36e9b5990c
VallereeJoseph <98525584+VallereeDJoseph@users.noreply.github.com>
2023-07-10 11:15:23 +0800

    Update index.js

--- finals/src/index.js
+++ finals/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Issues from './pages/issues';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-    <App />
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path='/issues' element={<Issues />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
