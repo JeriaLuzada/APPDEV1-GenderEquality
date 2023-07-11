d2f5cc5c2298845fbe38ba100ff1a1d56e241e79
VallereeDJoseph <20212590@s.ubaguio.edu>
2023-07-11 09:37:10 +0800

    update

---
+++ finals/src/pages/reference.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReferenceComponents from './referenceComponents';
+import Navbar from '../components/navbar';
+import '../components/App.css';
+import './App.css';
+
+function reference() {
+    return (
+        <div>
+            <Navbar />
+            <ReferenceComponents />
+        </div>
+    )
+}
+
+export default reference
---
+++ finals/src/pages/referenceComponents.js
@@ -0,0 +1,89 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+import { InfoOutlineIcon } from '@chakra-ui/icons';
+
+export default function Success() {
+    return (
+        <Box textAlign="center" py={10} px={6} bg="#FEC088">
+            <InfoOutlineIcon boxSize={'50px'} color={'green.500'} />
+            <Heading as="h2" size="xl" mt={6} mb={2}>
+                References
+            </Heading>
+            <Box textAlign="left" py={30} px={50}>
+
+
+                <Text color={'gray.800'}>
+                    [1] UNESCO. (2018). International technical guidance on sexuality education: An evidence-informed approach.
+                </Text>
+                <Text color={'gray.800'}>
+                    [2] Catalyst. (n.d.). Men Advocating Real Change (MARC).
+                </Text>
+                <Text color={'gray.800'}>
+                    [3] United Nations. (2019). Gender Equality in Education: A Statistical Picture.
+
+                </Text>
+                <Text color={'gray.800'}>
+                    [4] International Labour Organization. (2018). Global Wage Report 2018/19: What Lies Behind Gender Pay Gaps?
+                </Text>
+                <Text color={'gray.800'}>
+                    [5] World Bank. (2021). Women, Business, and the Law.
+                </Text>
+                <Text color={'gray.800'}>
+                    [6] McKinsey & Company. (2020). Diversity wins: How inclusion matters.
+
+                </Text>
+                <Text color={'gray.800'}>
+                    [7] United Nations Women. (n.d.). Ending violence against women.
+                </Text>
+                <Text color={'gray.800'}>
+                    [8] United Nations Entity for Gender Equality and the Empowerment of Women. (2013). Making Women's Work Visible: Gender-responsive Governance and Budgeting.
+                </Text>
+                <Text color={'gray.800'}>
+                    [9] International Labour Organization. (2018). Work-family balance policies and practice: Integration of the formal and informal sectors.
+                </Text>
+                <Text color={'gray.800'}>
+                    [10] Geena Davis Institute on Gender in Media. (n.d.). Research.
+                </Text>
+                <Text color={'gray.800'}>
+                    [11] United Nations Women. (n.d.). Promoting positive masculinities.
+                </Text>
+                <Text color={'gray.800'}>
+                    [12] Catalyst. (n.d.). Gender Non-Conformity.
+                </Text>
+                <Text color={'gray.800'}>
+                    [13] Promundo-US & UN Women. (2017). The Man Box: A study on being a young man in the US, UK, and Mexico.
+                </Text>
+                <Text color={'gray.800'}>
+                    [14] United Nations Population Fund. (2021). Promoting Gender Equality Among Young People in Eastern Europe and Central Asia.
+                </Text>
+                <Text color={'gray.800'}>
+                    [15] The Lancet. (2019). Gender equity in the health workforce: Analysis of 104 countries. The Lancet, 393(10189), 1039-1049. doi:10.1016/S0140-6736(18)33219-X
+                </Text>
+                <Text color={'gray.800'}>
+                    [16] World Health Organization: WHO. (2019). 10 key issues in ensuring gender equity in the global health workforce. www.who.int. https://www.who.int/news-room/feature-stories/detail/10-key-issues-in-ensuring-gender-equity-in-the-global-health-workforce
+                </Text>
+                <Text color={'gray.800'}>
+                    [17] Global Issues: Gender Equality and Women’s Empowerment. (n.d.). https://www.peacecorps.gov/educators/resources/global-issues-gender-equality-and-womens-empowerment/#18. United Nations. (n.d.). Universal Declaration of Human Rights | United Nations. https://www.un.org/en/about-us/universal-declaration-of-human-rights
+                </Text>
+                <Text color={'gray.800'}>
+                    [18] Laws and policies. (n.d.). UN Women – Headquarters. https://www.unwomen.org/en/how-we-work/gender-parity-in-the-united-nations/laws-and-policies
+                </Text>
+                <Text color={'gray.800'}>
+                    [19] United Nations. (2023, June 15). Advancing Gender Equality: UN launches new initiative. UN News. Retrieved from https://news.un.org/en/news/topic/womenhttps://www.freepik.com/search?format=search&query=gender%20equality
+                </Text>
+                <Text color={'gray.800'}>
+                    [20] World Health Organization. (n.d.). Gender equity in the health workforce: Analysis of 104 countries. Retrieved from https://www.who.int/hrh/resources/gender_equity-health_workforce_analysis/en/
+                </Text>
+                <Text color={'gray.800'}>
+                    [21] World Health Organization. (2021). Women in health and care. Retrieved from https://www.who.int/news-room/feature-stories/detail/women-in-health-and-care
+                </Text>
+                <Text color={'gray.800'}>
+                    [22] World Health Organization. (2021). Violence against women in the health sector. Retrieved from https://www.who.int/news-room/fact-sheets/detail/violence-against-women-in-the-health-sector
+                </Text>
+                <Text color={'gray.800'}>
+                    [23] United Nations Population Fund. (2020). Gender equality in the health workforce. Retrieved from https://www.unfpa.org/resources/gender-equality-health-workforce
+                </Text>
+            </Box>
+
+        </Box>
+    );
+}
