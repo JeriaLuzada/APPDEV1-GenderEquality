b751fa7a73a4ea5fb263766c8bb7c7911f7be954
VallereeJoseph <98525584+VallereeDJoseph@users.noreply.github.com>
2023-07-11 10:04:41 +0800

    Update issues.js

--- finals/src/pages/issues.js
+++ finals/src/pages/issues.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import IssuesComponents from './issuesComponents'
 import Navbar from '../components/navbar'
+import '../components/App.css';
+import './App.css';
 function issues() {
   return (
     <div>
