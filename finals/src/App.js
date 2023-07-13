fd6f7a92049b3992f4165365d128ab9f28e616be
JeriaLuzada_HUMCOM1_20212022 <97883231+20213477@users.noreply.github.com>
2023-07-13 14:17:50 +0800

    July13

--- finals/src/App.js
+++ finals/src/App.js
@@ -23,7 +23,6 @@ function App() {
         <Header/>
         <KeyIssues/>
         <Law />
-        <hr/>
         <Help/>
         <hr/>
         <News />
--- finals/src/components/navbar.js
+++ finals/src/components/navbar.js
@@ -5,39 +5,48 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = ({ darkMode, toggleDarkMode }) => {
     return (
-        <nav className="navbar-container navbar-expand-lg">
-            <a className="navbar-brand navbar-genderEquality" href="#">GENDER EQUALITY</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-container navbar-expand-lg">
+            <a className="navbar-brand navbar-genderEquality nav-mode" href="#">GENDER EQUALITY</a>
+            
+            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots toggle" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/></svg>
             </button>
-            <div className="collapse navbar-collapse button-panel" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto me-0 mb-auto mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active home" aria-current="page" href="/">Home</a>
+            <div className="offcanvas offcanvas-end button-panel nav-mode w-auto" id="navbarSupportedContent">
+            <div class="offcanvas-header px-4 pb-0">
+                <h4 className='me-2'>Gender Equality</h4>
+                <button type="button" className="btn-close ms-auto me-0 mb-0" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                
+            </div>
+            <hr className='d-lg-none'/>
+                <ul className="navbar-nav ms-auto me-1">
+                    <li className="nav-item ">
+                        <a className="nav-link fw-bold me-4"  href="/">Home</a>
                     </li>
-                    <li className="nav-item dropdown ms-4">
-                    <a className="nav-link dropdown-toggle about" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        About
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/issues">Key Issues</a></li>
-                        <li><a class="dropdown-item" href="#law">Laws and Policies</a></li>
-                        <li><a class="dropdown-item" href="/reference">Reference</a></li>
-                    </ul>
+                    <li className="nav-item dropdown me-4 ">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            About
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/issues">Key Issues</a></li>
+                            <li><a class="dropdown-item" href="#law">Laws and Policies</a></li>
+                            <li><a class="dropdown-item" href="/reference">Reference</a></li>
+                        </ul>
                     </li>
-                    <li className="nav-item ms-4">
-                        <a className="nav-link news-nav" href="#news">News</a>
+
+                    <li className="nav-item me-4">
+                        <a className="nav-link" href="#news">News</a>
                     </li>
-                    <li className="nav-item ms-4">
-                        <a className="nav-link contact" href="/contact">Contact</a>
+
+                    <li className="nav-item me-4">
+                        <a className="nav-link" href="/contact">Contact</a>
                     </li>
                 
-                    <div className="search-container ms-4">
+                    <li className="search-container me-4">
                         <input className="form-control search-input" type="search" placeholder="Search"/>
                         <FontAwesomeIcon className="search-icon" icon={faSearch} />
-                    </div>
+                    </li>
                     
-                    <button className='sun-svg ms-4' onClick={toggleDarkMode}>{darkMode ? <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-sun" viewBox="0 0 16 16"> <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/> </svg>: <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-sun" viewBox="0 0 16 16"> <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/> </svg>}
+                    <button className='sun-svg me-4 mt-1' onClick={toggleDarkMode}>{darkMode ? <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-sun" viewBox="0 0 16 16"> <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/> </svg>: <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-sun" viewBox="0 0 16 16"> <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/> </svg>}
                     </button>
                 </ul>
             </div>
